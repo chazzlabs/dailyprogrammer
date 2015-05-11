a75ca89f39f72691025e742fc48a1c5b96241e6b
charles.labas <cslabas@gmail.com>
2015-05-11 18:45:40 -0400

    Minor changes.  Added unit tests

--- easy/214/standard_deviation.go
+++ easy/214/standard_deviation.go
@@ -34,9 +34,25 @@ func calculateStdDeviation(values []float64) float64 {
     return math.Sqrt(variance)
 }
 
-func round(f float64, places int) (float64) {
-    shift := math.Pow(10, float64(places))
-    return math.Floor(f * shift) / shift;
+// From: https://gist.github.com/indytechcook/5706434
+func round(x float64, prec int) float64 {
+    var rounder float64
+	pow := math.Pow(10, float64(prec))
+	intermed := x * pow
+	_, frac := math.Modf(intermed)
+	intermed += .5
+	x = .5
+	if frac < 0.0 {
+		x = -.5
+		intermed -= 1
+	}
+	if frac >= x {
+		rounder = math.Ceil(intermed)
+	} else {
+		rounder = math.Floor(intermed)
+	}
+
+	return rounder / pow
 }
 
 func convertToFloatArray(stringValues []string) []float64 {
@@ -45,6 +61,7 @@ func convertToFloatArray(stringValues []string) []float64 {
     for _, stringValue := range stringValues {
         floatValue, err := strconv.ParseFloat(stringValue, 64)
 
+        // TODO: Research and implement error handling
         if err != nil {
             break
         }
--- easy/214/standard_deviation_test.go
+++ easy/214/standard_deviation_test.go
@@ -1,6 +1,22 @@
 package main
 
-import "testing"
+import (
+    "testing"
+)
+
+func sliceEqual(a, b[]float64) bool {
+    if len(a) != len(b) {
+        return false
+    }
+
+    for index, value := range a {
+        if value != b[index] {
+            return false
+        }
+    }
+
+    return true
+}
 
 func TestCalculateMean(t *testing.T) {
     testCases := []struct {
@@ -15,6 +31,72 @@ func TestCalculateMean(t *testing.T) {
 		result := calculateMean(testCase.inputValues)
 		if result != testCase.expectedMean {
 			t.Errorf("calculateMean(%q) == %f, want %f", testCase.inputValues, result, testCase.expectedMean)
+		}
+	}
+}
+
+func TestCalculateVariance(t *testing.T) {
+    testCases := []struct {
+		inputValues []float64
+        mean float64
+        expectedVariance float64
+	} {
+		{[]float64{5, 6, 11, 13, 19, 20, 25, 26, 28, 37}, 19.0, 95.6},
+        {[]float64{1, 2}, 1.5, 0.25},
+	}
+	for _, testCase := range testCases {
+		result := calculateVariance(testCase.inputValues, testCase.mean)
+		if result != testCase.expectedVariance {
+			t.Errorf("calculateVariance(%f) == %f, want %f", testCase.inputValues, result, testCase.expectedVariance)
+		}
+	}
+}
+
+func TestCalculateStdDevidation(t *testing.T) {
+    testCases := []struct {
+		inputValues []float64
+        expectedStdDeviation float64
+	} {
+		{[]float64{5, 6, 11, 13, 19, 20, 25, 26, 28, 37}, 9.777525249264253},
+	}
+	for _, testCase := range testCases {
+		result := calculateStdDeviation(testCase.inputValues)
+		if result != testCase.expectedStdDeviation {
+			t.Errorf("calculateStdDeviation(%q) == %f, want %f", testCase.inputValues, result, testCase.expectedStdDeviation)
+		}
+	}
+}
+
+func TestRound(t *testing.T) {
+    testCases := []struct {
+		inputValue float64
+        places int
+        expectedRoundedValue float64
+	} {
+        {10.12, 1, 10.1},
+        {10.12345, 4, 10.1235},
+		{10.0, 1, 10.0},
+	}
+	for _, testCase := range testCases {
+		result := round(testCase.inputValue, testCase.places)
+		if result != testCase.expectedRoundedValue {
+			t.Errorf("round(%f, %d) == %f, want %f", testCase.inputValue, testCase.places, result, testCase.expectedRoundedValue)
+		}
+	}
+}
+
+func TestConvertToFloatArray(t *testing.T) {
+    testCases := []struct {
+		inputArray []string
+        expectedArray []float64
+	} {
+        {[]string{"10", "11", "12"}, []float64{10.000000, 11.000000, 12.000000}},
+
+	}
+	for _, testCase := range testCases {
+		result := convertToFloatArray(testCase.inputArray)
+		if !sliceEqual(result, testCase.expectedArray) {
+			t.Errorf("convertToFloatArray(%q) == %f, want %f", testCase.inputArray, result, testCase.expectedArray)
 		}
 	}
 }
