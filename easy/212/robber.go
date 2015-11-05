ef91dccc78ee71c651e7b39b33ea9cfabe30ec32
charles.labas <cslabas@gmail.com>
2015-11-05 15:59:09 -0500

    Added gitignore. Leveraged more flag functionality in robber

--- easy/212/robber.go
+++ easy/212/robber.go
@@ -1,60 +1,60 @@
 package main
 
 import (
-    "fmt"
-    "unicode"
-    "strings"
-    "flag"
+	"flag"
+	"fmt"
+	"strings"
+	"unicode"
 )
 
-const Consonants = "BCDGFHJKLMNPQRSTVWXZbcdfghjklmnpqrstvwxz"
+const consonants = "BCDGFHJKLMNPQRSTVWXZbcdfghjklmnpqrstvwxz"
 
 func encode(inputString string) string {
-    var encodedString []rune
+	var encodedString []rune
 
-    for _, letter := range inputString {
-        encodedString = append(encodedString, letter)
+	for _, letter := range inputString {
+		encodedString = append(encodedString, letter)
 
-        if strings.ContainsRune(Consonants, letter) {
-            encodedString = append(encodedString, 'o')
-            encodedString = append(encodedString, unicode.ToLower(letter))
-        }
-    }
+		if strings.ContainsRune(consonants, letter) {
+			encodedString = append(encodedString, 'o')
+			encodedString = append(encodedString, unicode.ToLower(letter))
+		}
+	}
 
-    return string(encodedString)
+	return string(encodedString)
 }
 
 func decode(inputString string) string {
-    var decodedString []rune
-    inputRunes := []rune(inputString)
+	var decodedString []rune
+	inputRunes := []rune(inputString)
 
-    for index := 0; index < len(inputRunes); index++ {
-        letter := inputRunes[index]
+	for index := 0; index < len(inputRunes); index++ {
+		letter := inputRunes[index]
 
-        decodedString = append(decodedString, letter)
+		decodedString = append(decodedString, letter)
 
-        if strings.ContainsRune(Consonants, letter) {
-            index += 2
-        }
-    }
+		if strings.ContainsRune(consonants, letter) {
+			index += 2
+		}
+	}
 
-    return string(decodedString)
+	return string(decodedString)
 }
 
 func main() {
-    var decodeFlag = flag.Bool("d", false, "Usage: '-d <message>'")
-    var encodeFlag = flag.Bool("e", false, "Usage: '-e <message>'")
-    flag.Parse()
+	var decodeFlag = flag.Bool("d", false, "Usage: '-d <message>'")
+	var encodeFlag = flag.Bool("e", false, "Usage: '-e <message>'")
+	flag.Parse()
 
-    inputString := flag.Args()[0]
+	if *decodeFlag {
+		result := decode(flag.Args()[0])
+		fmt.Println(result)
+		return
+	} else if *encodeFlag {
+		result := encode(flag.Args()[0])
+		fmt.Println(result)
+		return
+	}
 
-    result := "Use '-d <message>' or '-e <message>'"
-
-    if *decodeFlag {
-        result = decode(inputString)
-    } else if *encodeFlag {
-        result = encode(inputString)
-    }
-
-    fmt.Println(result)
+	flag.PrintDefaults()
 }
